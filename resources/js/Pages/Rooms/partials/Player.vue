0e1676570228cef77931c2dcd830f61f8dd92018
Bastien Formery <1478542+bstnfrmry@users.noreply.github.com>
2024-02-27 23:00:12 +0100

    fix(player): Prevent player from overflowing

--- resources/js/Pages/Rooms/partials/Player.vue
+++ resources/js/Pages/Rooms/partials/Player.vue
@@ -154,7 +154,7 @@ const startCountdown = () => {
     </div>
     <div v-else class="w-full max-w-full">
       <div class="absolute top-0 left-0 z-10 h-4 rounded-r-lg rounded-tl-lg bg-gradient-to-br from-red-600 to-transparent transition-all duration-500 ease-linear" :style="'width:' + percent + '%; max-width: 18%'" />
-      <div class="shine absolute h-4 rounded-r-lg rounded-tl-lg bg-gradient-to-br from-purple-300 to-purple-400 transition-all duration-500 ease-linear" :style="'width:' + percent + '%'" />
+      <div class="shine absolute h-4 rounded-r-lg rounded-tl-lg bg-gradient-to-br from-purple-300 to-purple-400 transition-all duration-500 ease-linear" :style="'width:' + percent + '%; max-width: 100%'" />
     </div>
   </div>
   <UserGestureModal @play="triggerUserGesture" />
